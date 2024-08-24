b1c78e21421cd08d25fef7ced7aef71965f29ef5
weihongyan0729 <weihongyan0729@gmail.com>
2024-08-24 14:34:01 +0800

    v1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'education', 'awards']
+const section_names = ['home', 'education', 'test', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
